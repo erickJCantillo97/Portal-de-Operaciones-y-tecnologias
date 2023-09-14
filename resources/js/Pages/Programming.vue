17a8e4df73bc805db9b88e29aad6fdde7c8972bb
gbuelvas <gbuelvas@cotecmar.com>
2023-09-14 08:42:23 -0500

    cambio en programacion

--- resources/js/Pages/Programming.vue
+++ resources/js/Pages/Programming.vue
@@ -118,37 +118,14 @@ const redondear = (value )=> {
                 </template>
 
                 <template #header>
-                    <div class="w-full mb-2">
-                        <div class="flex justify-end h-8 mb-2 space-x-4">
-                            <span class="shadow-xl">
-                                <button type="button" :class="optionValue == 'today' ? 'bg-sky-500 text-white':'bg-white hover:bg-sky-200 text-gray-90'" @click="getTask('today')"
-                                    class="relative inline-flex items-center rounded-l-md  px-3 py-2 text-sm font-semibold 0 ring-1 ring-inset ring-gray-300 focus:z-10">Hoy</button>
-                                <button type="button" :class="optionValue == 'tomorrow' ? 'bg-sky-500 text-white':'bg-white hover:bg-sky-200 text-gray-90'"  @click="getTask('tomorrow')"
-                                    class="relative -ml-px inline-flex items-center  px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300 focus:z-10">Mañana</button>
-                                <button type="button" :class="optionValue == 'week' ? 'bg-sky-500 text-white':'bg-white hover:bg-sky-200 text-gray-90'" @click="getTask('week')"
-                                    class="relative -ml-px inline-flex items-center rounded-r-md px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300  focus:z-10">Semana</button>
-                            </span>
-                            <span class="p-float-label">
-                                <calendar id="calendar" selectionMode="range" v-model="dates"
-                                    v-on:date-select="getTask('range')"
-                                    class="focus:ring-2 focus:ring-indigo-600 focus:ring-inset shadow-xl h-9"></calendar>
-                                <label for="calendar">Rango de fechas</label>
-                            </span>
-                            <!-- <span class="p-float-label">
-                                <Calendar id="calendar"
-                                    class="block text-gray-900 border-0 rounded-md placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    :unstyled="false" v-on:date-select="getTask()" v-model="dates" selectionMode="range"
-                                    :manualInput="false" />
-                                <label for="calendar">Rango de fechas</label>
-                            </span> -->
-                        </div>
-                        <div class="alturah8 flex h-8 space-x-4">
-                            <Button icon="pi pi-filter-slash" @click="clearFilter()" type="button" text=""
+                    <div class="w-full mb-2 flex justify-between">
+                        <div class="alturah8 flex space-x-4">
+                            <Button icon="pi pi-filter-slash shadow-xl" @click="clearFilter()" type="button" text=""
                                 severity="primary" class="hover:bg-primary ">
                             </Button>
                             <span class="p-float-label">
                                 <InputText id="buscar" v-model="filters.global.value"  type="search"
-                                    class="block text-gray-900 border-0 rounded-md placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                    class="alturah8 shadow-xl block text-gray-900 rounded-md placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                                 <label for="buscar">Buscar...</label>
                             </span>
                             <!-- <div class="relative flex rounded-md shadow-sm">
@@ -159,6 +136,29 @@ const redondear = (value )=> {
                                     class="block w-10/12 py-4 pl-10 text-gray-900 border-0 rounded-md ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                     v-model="filters.global.value" placeholder="Buscar..." />
                             </div> -->
+                        </div>
+                        <div class="flex justify-end alturah8 mb-2 space-x-4">
+                            <span class="shadow-xl alturah8">
+                                <button type="button" :class="optionValue == 'today' ? 'bg-sky-500 text-white':'bg-white hover:bg-sky-200 text-gray-90'" @click="getTask('today')"
+                                    class="alturah8 relative inline-flex items-center rounded-l-md  px-3 py-2 text-sm font-semibold 0 ring-1 ring-inset ring-gray-300 focus:z-10">Hoy</button>
+                                <button type="button" :class="optionValue == 'tomorrow' ? 'bg-sky-500 text-white':'bg-white hover:bg-sky-200 text-gray-90'"  @click="getTask('tomorrow')"
+                                    class="alturah8 relative -ml-px inline-flex items-center  px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300 focus:z-10">Mañana</button>
+                                <button type="button" :class="optionValue == 'week' ? 'bg-sky-500 text-white':'bg-white hover:bg-sky-200 text-gray-90'" @click="getTask('week')"
+                                    class="alturah8 relative -ml-px inline-flex items-center rounded-r-md px-3 py-2 text-sm font-semibold ring-1 ring-inset ring-gray-300  focus:z-10">Semana</button>
+                            </span>
+                            <span class="p-float-label">
+                                <calendar id="calendar" selectionMode="range" v-model="dates"
+                                    v-on:date-select="getTask('range')"
+                                    class="alturah8 focus:ring-2 focus:ring-indigo-600 focus:ring-inset shadow-xl h-9"></calendar>
+                                <label for="calendar">Rango de fechas</label>
+                            </span>
+                            <!-- <span class="p-float-label">
+                                <Calendar id="calendar"
+                                    class="block text-gray-900 border-0 rounded-md placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    :unstyled="false" v-on:date-select="getTask()" v-model="dates" selectionMode="range"
+                                    :manualInput="false" />
+                                <label for="calendar">Rango de fechas</label>
+                            </span> -->
                         </div>
                     </div>
                 </template>
