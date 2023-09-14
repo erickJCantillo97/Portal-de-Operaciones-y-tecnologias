299eab260a5f75ad5c98efed48941d4f1adf8671
gbuelvas <gbuelvas@cotecmar.com>
2023-09-14 14:54:26 -0500

    mejora visual programacion

--- resources/js/Components/Avatars.vue
+++ resources/js/Components/Avatars.vue
@@ -16,10 +16,10 @@ const props = defineProps({
     },
     class: {
         type: String,
-        default: 'rounded-full h-8',
+        default: 'inline-block h-8 w-8 rounded-full ring-1 ring-blue-800 hover:z-20 hover:scale-125',
     }
 });
-const assignments = ref();
+const assignments = ref([]);
 
 axios.get(route('get.assignments.task', props.taskId)).then(response => {
     assignments.value = response.data.assignments;
@@ -28,12 +28,15 @@ axios.get(route('get.assignments.task', props.taskId)).then(response => {
 </script>
 
 <template>
-    <div class="flex" v-if="assignments">
+    <div class="flex -space-x-2 overflow-hidden p-1 " v-if="assignments.length > 0">
         <img v-for="assignment in assignments"
-            v-tooltip.top="{ value: `<h4 class='text-blue-800 text-center bg-blue-200'>` + (assignment.name) + `</h4>`, escape: true, class: 'custom-error' }"
+            v-tooltip.top="{ value: `<h4 class='text-blue-800 text-center bg-blue-200'>` + (assignment.name) + ` : `+(assignment.units/100)+ ` UND</h4>`, escape: true, class: 'custom-error' }"
             :class="class" class=""
             :src="'https://ui-avatars.com/api/?name=' + assignment.name + '&color=' + props.color + '&background=' + props.background"
             alt="imagen">
+    </div>
+    <div v-else class="text-red-600 font-bold text-sm text-center uppercase italic animate-pulse">
+        Sin asignaciones
     </div>
 </template>
 
