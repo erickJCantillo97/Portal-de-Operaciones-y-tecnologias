59a415bb869e132953ff957a5e47ef1ae6e5c2ef
gbuelvas <gbuelvas@cotecmar.com>
2023-10-04 09:28:05 -0500

    se habilita boton de cancelar en buzon de sugerencias

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -227,7 +227,7 @@
                                         <div class="flex flex-shrink-0 justify-end px-4 py-4">
                                             <button type="button"
                                                 class="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                                                @click="open = false">Cancelar</button>
+                                                @click="sugerenciaVisible = false">Cancelar</button>
                                             <button type="submit" @click="enviaSugerencia()"
                                                 class="ml-4 inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Guardar</button>
                                         </div>
