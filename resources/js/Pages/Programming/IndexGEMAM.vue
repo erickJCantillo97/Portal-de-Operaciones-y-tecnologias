b9a52706bb1e963dbc74e923998ff39e1e764010
Ronny Gutierrez <rgutierrez@cotecmar.com>
2023-10-05 11:46:17 -0500

    suerte 1

--- resources/js/Pages/Programming/IndexGEMAM.vue
+++ resources/js/Pages/Programming/IndexGEMAM.vue
@@ -18,9 +18,11 @@ import { applyDrag, generateItems } from "@/composable/helpers.js";
 
 //#region Draggable
 const data = ref({
-    items1: generateItems(2, (i) => ({
-        id: "1"+i,
-        data: `Personal:\ncargo- ${i}`
+    items1: generateItems(10, (i) => ({
+        id: "1" + i,
+        data: `Personal:\ncargo- ${i}`,
+        imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     })),
     items2: [],
 })
@@ -31,11 +33,11 @@ const onDrop = (collection, dropResult) => {
 }
 
 const getChildPayload1 = (index) => {
-    return data.value.items1[index-1];
+    return data.value.items1[index - 1];
 }
 
 const getChildPayload2 = (index) => {
-    return data.value.items2[index-1];
+    return data.value.items2[index - 1];
 }
 
 //#endregion
@@ -167,7 +169,6 @@ const items = ref([
 ]);
 
 //#endregion
-
 </script>
 <style scoped>
 .copy {
@@ -195,28 +196,18 @@ const items = ref([
                 </div>
             </div>
             <div>
-                <div class="flex items-center my-4 space-x-2">
-                    <div class="hidden w-1/12 md:block">
-                        <Button icon="pi pi-filter-slash shadow-xl" @click="filterProject()" type="button" text=""
-                            severity="primary" class="hover:bg-primary ">
-                            Ver Todos
-                        </Button>
-                    </div>
-                    <div class="flex w-11/12 p-1 space-x-2 overflow-x-auto shadow-sm shadow-primary rounded-xl snap-x">
-                        <ProjectCard v-for="project in projects" :projectId=project :menu=false clases="cursor-pointer"
-                            @click="filterProject(project)" :activo="filters['project.id'].value == project" />
-                    </div>
-                </div>
 
             </div>
 
             <div class="copy">
-                <div class="grid w-5/6 h-screen grid-cols-2 overflow-auto border rounded-md shadow-lg custom-scroll item sm:grid-cols-2">
+                <div
+                    class="grid w-5/6 h-screen grid-cols-2 overflow-auto border rounded-md shadow-lg custom-scroll item sm:grid-cols-2">
                     <div v-for="task in tasks"
                         class="flex flex-col justify-between p-2 m-2 border border-blue-800 rounded-md shadow-lg">
                         <div class="flex">
                             <p class="w-1/4 mr-1 font-bold">Actividad:</p>
-                            <p v-tooltip.top="task.name" class="block w-full overflow-hidden whitespace-nowrap text-ellipsis">{{ task.name }}</p>
+                            <p v-tooltip.top="task.name"
+                                class="block w-full overflow-hidden whitespace-nowrap text-ellipsis">{{ task.name }}</p>
                         </div>
                         <div class="block text-xs">
                             <div class="flex w-1/2">
@@ -230,7 +221,8 @@ const items = ref([
                                 </p>
                             </div>
                         </div>
-                        <Container group-name="1" class="h-20 p-2 mt-2 mb-2 overflow-auto bg-blue-200 rounded-md custom-scroll"
+                        <Container group-name="1"
+                            class="h-20 p-2 mt-2 mb-2 overflow-auto bg-blue-200 rounded-md custom-scroll"
                             @drop="onDrop('items2', $event)">
                             <div class="grid grid-cols-2 gap-1">
                                 <div v-for="item in data.items2" :key="item.id">
@@ -243,15 +235,16 @@ const items = ref([
                     </div>
                 </div>
                 <div
-                    class="overflow-hidden bg-white divide-y divide-gray-100 shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl">
+                    class="w-96 overflow-hidden bg-white divide-y h-screen divide-gray-100 shadow-lg ring-1 ring-gray-900/5 sm:rounded-xl">
+                    <h2 class="font-semibold leading-6 text-center capitalize text-primary">Personal</h2>
                     <Container
-                        class="relative flex flex-col justify-between px-1 py-1 overflow-y-auto h-96 gap-x-2 hover:bg-gray-50 sm:px-6"
+                        class="relative flex flex-col gap px-1 py-1 overflow-y-auto h-full custom-scroll gap-x-2  sm:px-1"
                         behaviour="copy" group-name="1" :get-child-payload="getChildPayload1">
-                        <h2 class="font-semibold leading-6 text-center capitalize text-primary">Personal</h2>
                         <Draggable v-for="item in data.items1" :key="item.id"
-                            class="relative flex justify-between px-4 py-5 gap-x-6 hover:bg-gray-50 sm:px-6">
-                            <div class="flex min-w-0 gap-x-4">
-                                <img class="flex-none w-12 h-12 rounded-full bg-gray-50" alt="profile-photo" />
+                            class="relative flex justify-between py-2 mt-2 shadow-md cursor-pointer  sm:rounded-xl hover:bg-blue-200 pl-2">
+                            <div class="flex min-w-0">
+                                <!-- <img class="flex-none w-12 h-12 rounded-full bg-gray-50" :src="item.imageUrl"
+                                    alt="profile-photo" /> -->
                                 <div class="flex-auto min-w-0">
                                     <p class="text-sm font-semibold leading-6 text-gray-900">
                                         <a>
@@ -282,7 +275,7 @@ const items = ref([
                 <ul role="list"
                     class="overflow-hidden bg-white divide-y divide-gray-100 shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl">
                     <li v-for="person in people" :key="person.email"
-                        class="relative flex justify-between px-4 py-5 gap-x-6 hover:bg-gray-50 sm:px-6">
+                        class="relative flex justify-between px-4 py-5 gap-x-6  sm:px-6">
                         <div class="flex min-w-0 gap-x-4">
                             <img class="flex-none w-12 h-12 rounded-full bg-gray-50" :src="person.imageUrl" alt="" />
                             <div class="flex-auto min-w-0">
