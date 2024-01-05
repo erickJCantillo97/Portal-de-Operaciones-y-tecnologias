d863bcb8cf0dc281a727c4d62c74d571a8e90de8
gbuelvas <gbuelvas@cotecmar.com>
2024-01-05 07:23:00 -0500

    Actualizando Vue y Vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path'
 
 export default defineConfig({
     plugins: [
@@ -21,5 +22,5 @@ export default defineConfig({
             }
         }),
     ],
-
+    resolve: { alias: { vue: path.resolve('./node_modules/vue', 'dist', 'vue.runtime.esm-bundler.js') } }
 });
