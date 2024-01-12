db781a2e8812ce3f740ded747bb73dc9fe1c15a3
gbuelvas <gbuelvas@cotecmar.com>
2024-01-12 11:11:15 -0500

    Se crean vistas y componentes estructurales de Dashboards

---
+++ resources/js/Pages/Dashboards/Estimaciones.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TablaEstimaciones from '@/Pages/Dashboards/Estimaciones/TablaEstimaciones.vue'
+
+</script>
+
+
+<template>
+    <TablaEstimaciones />
+</template>
---
+++ resources/js/Pages/Dashboards/Proyectos.vue
@@ -0,0 +1,29 @@
+<script setup>
+import CustomDataTable from '@/Components/CustomDataTable.vue';
+import { router } from "@inertiajs/vue3";
+import { onMounted, ref } from 'vue';
+
+const columnas = [
+    { field: 'name', header: 'Proyecto' },
+    { field: 'avance', header: 'Ejecución' },
+    { field: 'contrato', header: 'Contrato' },
+    { field: 'costo', header: 'Valor Venta' },
+    { field: 'fechaF', header: 'Fin Producción' },
+]
+//#region Botones de CustomDatatable
+const buttons = [
+    { event: 'showProgramming', severity: 'success', class: '', icon: 'fa-solid fa-list-check', text: true, outlined: false, rounded: false },
+    { event: 'showGantt', severity: 'primary', class: '', icon: 'fa-solid fa-list-check', text: true, outlined: false, rounded: false },
+    // { event: 'deleteClic', severity: 'danger', icon: 'fa-solid fa-trash', class: '!h-8', text: true, outlined: false, rounded: false },
+]
+const projects = ref([])
+onMounted(() => {
+    projects.value = []
+});
+</script>
+<template>
+    <CustomDataTable title="Proyectos" :data="projects" :columnas="columnas" :actions="buttons"
+        @showProgramming="router.get(route('programming'), { id: $event.data.project_id })"
+        @showGantt="router.get(route('createSchedule.create', $event.data.project_id))">
+    </CustomDataTable>
+</template>
