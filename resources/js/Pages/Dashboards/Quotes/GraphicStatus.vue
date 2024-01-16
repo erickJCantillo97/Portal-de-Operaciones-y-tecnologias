4fb83cd04d60aa4c133b9d15c8f2a555debe3325
gbuelvas <gbuelvas@cotecmar.com>
2024-01-16 10:28:09 -0500

    Se completan componentes de quotes

--- resources/js/Pages/Dashboards/Quotes/GraphicStatus.vue
+++ resources/js/Pages/Dashboards/Quotes/GraphicStatus.vue
@@ -2,9 +2,12 @@
 import { ref } from 'vue';
 import axios from "axios";
 import VueApexCharts from 'vue3-apexcharts';
+import Empty from '@/Components/Empty.vue';
+import Loading from '@/Components/Loading.vue';
 
 const series = ref([])
 const loading = ref(true)
+const empty = ref()
 const chartOptions = ref({
     title: {
         text: 'Estimaciones por estado',
@@ -58,11 +61,17 @@ const getQuotesMadurity = () => {
     loading.value = true
     axios.get(route("get.quotes.status.week")).then((res) => {
         const data = Object.values(res.data)
-        series.value = [{
-            'name': 'Cantidad',
-            'data': data.map(quote => (parseInt(quote.value)))
-        }]
-        chartOptions.value.xaxis.categories = Object.values(res.data).map(status => status.status + '(s)');
+        if (data.length == 0) {
+            empty.value = true
+        } else {
+
+            series.value = [{
+                'name': 'Cantidad',
+                'data': data.map(quote => (parseInt(quote.value)))
+            }]
+            chartOptions.value.xaxis.categories = Object.values(res.data).map(status => status.status + '(s)');
+            empty.value = false
+        }
         loading.value = false
     });
 }
@@ -71,6 +80,7 @@ getQuotesMadurity()
 
 </script>
 <template>
-    <VueApexCharts type="bar" v-if="!loading" :options="chartOptions" :series="series" width="380"
-        class="flex justify-center" />
+    <Loading v-if="loading"></loading>
+    <Empty v-else-if="empty" message="Aun sin estados que mostrar"></Empty>
+    <VueApexCharts type="bar" v-else :options="chartOptions" :series="series" width="380" class="flex justify-center" />
 </template>
