1cb5589f6676c3be1b21425a493affe300010b13
ecantillo <ecantillo@cotecmar.com>
2024-01-16 13:04:07 -0500

    Card de estimadores lista

--- routes/Customs/Dashboards.php
+++ routes/Customs/Dashboards.php
@@ -10,4 +10,5 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
     Route::get('getQuotesManurity', [DashboardEstimacionesController::class, 'getQuotesManurity'])->name('get.quotes.manurity');
     Route::get('getStatusWeek', [DashboardEstimacionesController::class, 'getStatusWeek'])->name('get.quotes.status.week');
     Route::get('getAvgManurities', [DashboardEstimacionesController::class, 'getAvgManurities'])->name('get.avg.manurities');
+    Route::get('getEstimatorData', [DashboardEstimacionesController::class, 'getEstimatorData'])->name('get.estimator.data');
 });
