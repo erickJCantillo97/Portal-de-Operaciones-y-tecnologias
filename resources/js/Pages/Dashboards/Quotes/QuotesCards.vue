0c8998889f3e119a3182809deaa782264231963a
rgutierrez <rgutierrez@cotecmar.com>
2024-01-17 11:27:14 -0500

    Ajustando componente carrusel de estimaciones

--- resources/js/Pages/Dashboards/Quotes/QuotesCards.vue
+++ resources/js/Pages/Dashboards/Quotes/QuotesCards.vue
@@ -1,11 +1,31 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import Carousel from 'primevue/carousel'
+
+// const props = defineProps({
+//   info: {
+//     type: Array,
+//     required: true
+//   }
+// })
+
+// const quoteInfo = ref([])
+
+// const getQuoteInfo = async() => {
+//   await axios.get(route('fet.quotes.info'))
+//     .then((res) => {
+//       quoteInfo.value = res.data.message
+//     })
+// }
+
+// onMounted(()=> {
+//   getQuoteInfo()
+// })
 
 const item = [
   {
     message: '9 Dias en Promedio',
-    submessage: 'De Respuesta del Estimador',
+    submessage: 'De respuesta del departamento',
     icon: 'fa-solid fa-gauge'
   },
   {
@@ -14,14 +34,14 @@ const item = [
     icon: 'fa-solid fa-calendar-week'
   },
   {
-    message: '9 Dias en Promedio',
-    submessage: 'De Respuesta del Estimador',
-    icon: 'fa-solid fa-calendar-days'
-  },
-  {
     message: '3 Ofertas',
     submessage: 'Solicitadas este año',
     icon: 'fa-solid fa-layer-group'
+  },
+  {
+    message: '2 Ofertas',
+    submessage: 'Contratadas este año',
+    icon: 'fa-solid fa-calendar-days'
   },
 ]
 
@@ -50,11 +70,18 @@ const responsiveOptions = ref([
 </script>
 
 <template>
-  <Carousel :value="item" :numVisible="2" :numScroll="2" circular :autoplayInterval="60000">
+  <Carousel :value="item" :numVisible="2" :numScroll="2" circular :autoplayInterval="60000" :pt="{
+    indicators: '!hidden',
+    // indicatorButton: '!rounded-lg',
+    // indicatorButton: ({ state }) => ({
+    //   class: state.focused ? '!bg-blue-100' : '!bg-orange-800',
+    // })
+  }">
     <template #item="slotProps">
-      <div class="items-center gap-x-2 mt-2 mb-2 border rounded-lg">
+      <section class="items-center gap-x-2 mt-2 mb-2">
         <div class="col-span-1">
-          <div class="flex p-2 m-2 text-center justify-center border rounded-lg hover:border-b-2 hover:border-blue-800">
+          <div class="flex p-2 m-2 text-center justify-center border rounded-lg
+            hover:border-l-0 hover:border-t-0 hover:border-r-0 hover:border-b-2 hover:border-blue-800">
             <div class="size-12 mr-4">
               <i class="text-4xl text-blue-800" :class="slotProps.data.icon"></i>
             </div>
@@ -64,7 +91,7 @@ const responsiveOptions = ref([
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </template>
   </Carousel>
 </template>
