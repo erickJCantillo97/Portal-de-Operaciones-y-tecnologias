e3d02638661a6935965cbc6bef05a75a9d59070b
ecantillo <ecantillo@cotecmar.com>
2024-02-08 11:41:46 -0500

    milestone

---
+++ app/Http/Controllers/Projects/MilestoneController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Projects;
+
+use App\Http\Controllers\Controller;
+use App\Models\Projects\Milestone;
+use Exception;
+use Illuminate\Http\Request;
+
+class MilestoneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            //
+        ]);
+
+        try {
+            Milestone::create($validateData);
+        } catch (Exception $e) {
+            return back()->withErrors('message', 'Ocurrio un Error Al Crear : ' . $e);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Milestone $milestone)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Milestone $milestone)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Milestone $milestone)
+    {
+        $validateData = $request->validate([
+            //
+        ]);
+
+        try {
+            $milestone->update($validateData);
+        } catch (Exception $e) {
+            return back()->withErrors('message', 'Ocurrio un Error Al Actualizar : ' . $e);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Milestone $milestone)
+    {
+        try {
+            $milestone->delete();
+        } catch (Exception $e) {
+            return back()->withErrors('message', 'Ocurrio un Error Al eliminar : ' . $e);
+        }
+    }
+}
