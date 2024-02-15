af72c56279f34d522996cfca2cbbca7344f67d17
rgutierrez <rgutierrez@cotecmar.com>
2024-02-15 09:12:46 -0500

    Añadiendo datos del buque en ProjectOverview

---
+++ resources/js/Components/DescriptionItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  options: Array,
+  name: {
+    String,
+    default: 'Name'
+  }
+})
+</script>
+<template>
+  <div class="border border-solid rounded-lg p-2 mb-2">
+    <dl class="divide-y divide-gray-200 border-b border-t border-gray-200">
+      <div v-for="item in options.length" :key="item.id" class="flex justify-between py-3 text-sm font-medium">
+        <dt class="text-gray-900">{{ item.name }}:</dt>
+        <dd class="text-gray-500 uppercase">{{ item.type }}</dd>
+      </div>
+    </dl>
+  </div>
+</template>
