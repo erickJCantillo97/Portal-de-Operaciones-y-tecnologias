f888a9c69dfcdbfec02534ee83ac84c6b53240b5
ecantillo <ecantillo@cotecmar.com>
2024-02-27 11:55:49 -0500

    cambio en el mapa de caracteres

--- app/Http/Controllers/GD/ManagerDocumentController.php
+++ app/Http/Controllers/GD/ManagerDocumentController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\GD;
 
 use App\Http\Controllers\Controller;
 use App\Models\GD\FileManagerDocument;
+use App\Models\Projects\Project;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
@@ -79,5 +81,55 @@ class ManagerDocumentController extends Controller
     public function getProjectsGD()
     {
         return Inertia::render('GesDoc/GesDocView');
+    }
+
+    public function uploadApi(Request $request)
+    {
+        $validateData = $request->validate([
+            'project_sap' => 'string|required',
+            'project_name' => 'string|required',
+            'tipologia_id' => 'numeric|required',
+            'tipologia_name' => 'string|required',
+            'gerencia' => 'string|required',
+            'file' => 'file|required'
+        ]);
+        try {
+            $file = $validateData['file'];
+
+            $project = Project::firstOrNew([
+                'SAP_code' => $validateData['project_sap'],
+                'gerencia' => $validateData['gerencia']
+            ]);
+            $project->name = $validateData['project_name'];
+            $project->save();
+
+            $filePath = Storage::putFileAs(
+                'public/GestionDocumental/proyectos/' . $project->id,
+                $file,
+                $file->getClientOriginalName() . '_' . Carbon::now()->format('Y-m-d') . '_' . $validateData['tipologia_id'] . '_' . FileManagerDocument::count() + 1 . '.' . $file->getClientOriginalExtension()
+            );
+
+            // Obtiene el número de páginas
+            $texto_pdf = file_get_contents($file);
+            $num_page = preg_match_all("/\\/Page\\W/", $texto_pdf, $dummy);
+            $fileManager = FileManagerDocument::create([
+                'user_id' => 4,
+                'project_id' => $project->id,
+                'tipologia_id' => $validateData['tipologia_id'],
+                'tipologia_name' => $validateData['tipologia_name'],
+                'filePath' => str_replace('public', 'storage', $filePath),
+                'file_size' => $file->getSize(),
+                'name_user' => 'Gabriel Diaz',
+                'num_folios' => $num_page
+            ]);
+
+            return response()->json([
+                'message' => 'Archivo Subido Exitosamente'
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'Hubo un Error al cargar los datos: ' . $e
+            ], 500);
+        }
     }
 }
