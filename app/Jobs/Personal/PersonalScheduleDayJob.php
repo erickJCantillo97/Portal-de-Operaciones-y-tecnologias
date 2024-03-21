dbba6c158f92ed0de2b71e8fb24373c0556350ec
ecantillo <ecantillo@cotecmar.com>
2024-03-21 11:31:32 -0500

    añadidas las tareas de la semana

--- app/Jobs/Personal/PersonalScheduleDayJob.php
+++ app/Jobs/Personal/PersonalScheduleDayJob.php
@@ -2,8 +2,14 @@
 
 namespace App\Jobs\Personal;
 
+use App\Models\Gantt\Task;
+use App\Models\Personal\Employee;
+use App\Models\Personal\Personal;
+use App\Models\Schedule;
 use App\Models\ScheduleTime;
 use App\Models\Views\DetailScheduleTime;
+use App\Models\VirtualTask;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -18,7 +24,9 @@ class PersonalScheduleDayJob implements ShouldQueue
 
     public $date;
     public $personal_id;
-    public $task_id;
+    public $persona;
+    public $task;
+
     /**
      * Create a new job instance.
      */
@@ -26,7 +34,8 @@ class PersonalScheduleDayJob implements ShouldQueue
     {
         $this->date = $date;
         $this->personal_id = $personal_id;
-        $this->task_id = $task_id;
+        $this->persona = Employee::where('Num_SAP', $personal_id)->first();
+        $this->task =  VirtualTask::find($task_id);
     }
 
     /**
@@ -38,11 +47,36 @@ class PersonalScheduleDayJob implements ShouldQueue
 
         if (count($horarios) == 1) {
             $horario = $horarios->first();
-            if ($horario->idTask == $this->task_id) {
+            if ($horario->idTask == $this->task->id) {
                 ScheduleTime::where('id', $horario->idScheduleTime)->update([
-                    'hora_inicio' => '07:00:00.0000000',
-                    'hora_fin' => '16:30:00.0000000',
+                    'hora_inicio' => $this->task->project->shiftObject->startShift,
+                    'hora_fin' => $this->task->project->shiftObject->endShift,
                 ]);
+            }
+        } else {
+            for ($i = 0; $i < count($horarios); $i++) {
+                if ($i == 0) {
+                    if ($horarios[0]->hora_incio > $this->task->project->shiftObject->startShift) {
+                        if ($horarios[0]->idTask == $this->task->id) {
+                            ScheduleTime::where('id', $horarios[0]->idScheduleTime)->update([
+                                'hora_inicio' => $this->task->project->shiftObject->startShift,
+                            ]);
+                        } else {
+                            $schedule = Schedule::firstOrNew([
+                                'task_id' => $this->task->id,
+                                'employee_id' => $this->personal_id,
+                                'name' => $this->persona->Nombres_Apellidos,
+                                'fecha' => Carbon::parse($this->date)->format('Y-m-d'),
+                            ]);
+                            $schedule->save();
+                            ScheduleTime::create([
+                                'schedule_id' => $schedule->id,
+                                'hora_inicio' => Carbon::parse($this->task->project->shiftObject->startShift)->format('H:i'),
+                                'hora_fin' => $horarios[0]->hora_incio,
+                            ]);
+                        }
+                    }
+                }
             }
         }
     }
