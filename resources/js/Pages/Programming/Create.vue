244d85fc3887e6c01c7f86d5e26749ad6744d30e
gbuelvas <gbuelvas@cotecmar.com>
2024-03-21 15:30:33 -0500

    programming: Se agrega funcion para validar colisiones. pendiente enviar si es ultima colision

--- resources/js/Pages/Programming/Create.vue
+++ resources/js/Pages/Programming/Create.vue
@@ -19,6 +19,7 @@ import Breadcrumb from 'primevue/breadcrumb';
 import { useToast } from "primevue/usetoast";
 import { useConfirm } from "primevue/useconfirm";
 import InputSwitch from 'primevue/inputswitch';
+import axios from 'axios';
 const toast = useToast();
 const confirm = useConfirm();
 // const { hasRole, hasPermission } = usePermissions()
@@ -172,13 +173,7 @@ const employeeDialog = (item) => {
         })
 }
 
-const formColision = ref({
-    actionType: null, // accion a realizar: 1: mantener, 2: Reemplazar , 3: Reemplazar todo en una fecha, 4: mantener todas las colisiones en una fecha, 5: Mantener todas las tareas, 6: reemplazar todas las colisiones
-    idTask: null,
-    startShift: null, // hora de inicio de la nueva tarea
-    endShift: null, // hora final de la nueva tarea
-    details: [] //arreglo especificando el id de la tabla schedule_times EJ: [1,2,3,4]
-});
+
 
 const formEditHour = ref({
     userName: null, //nombre de la persona
@@ -274,29 +269,45 @@ const save = async () => {
                 }
                 // toast.add({ severity: 'danger', group: "customToast", text: res.data.mensaje, life: 2000 })
             }
-            console.log(res);
             formEditHour.value.loading = false
         }).catch((error) => {
             console.log(error)
             toast.add({ severity: 'error', group: "customToast", text: 'Error no controlado', life: 2000 })
         });
 }
-//# region popup confirmacion coliciones
 
-const confirm1 = (event, data, option) => {
+
+//# region popup confirmacion coliciones
+const formColision = ref({
+    actionType: null, // accion a realizar: 1: Reemplazar , 2: Omitir
+    idTask: null,// idTask de la Nueva tarea
+    scheduleTime: null, // id del schedule time
+    endSchedule: null, // se manda true si es la ultima colisión
+    startShift: null, // hora de inicio de la nueva tarea
+    endShift: null, // hora final de la nueva tarea
+});
+
+async function confirm1(event, scheduleTime, option, data) {
     formColision.value.idTask = task.value.id
     formColision.value.startShift = format24h(task.value.shift.startShift)
     formColision.value.endShift = format24h(task.value.shift.endShift)
+
     if (option == 'omit') {
+        scheduleTime.status = option
         formColision.value.actionType = 1
-        formColision.value.details = [data.idScheduleTime]
-        toast.add({ severity: 'info', group: "customToast", text: 'Omitida', life: 3000 });
+        formColision.value.scheduleTime = scheduleTime.idScheduleTime
+        if (resolveCollision(formColision)) {
+            toast.add({ severity: 'info', group: "customToast", text: 'Omitida', life: 3000 });
+        }
     } else if (option == 'remplace') {
+        scheduleTime.status = option
         formColision.value.actionType = 2
-        formColision.value.details = [data.idScheduleTime]
-        toast.add({ severity: 'info', group: "customToast", text: 'Remplazada', life: 3000 });
+        formColision.value.scheduleTime = scheduleTime.idScheduleTime
+        if (resolveCollision(formColision)) {
+            toast.add({ severity: 'info', group: "customToast", text: 'Omitida', life: 3000 });
+        }
     } else {
-        formColision.value.details = data.map(obj => obj.idScheduleTime);
+        // formColision.value.details = data.map(obj => obj.idScheduleTime);
         confirm.require({
             target: event.currentTarget,
             message: '¿Está totalmente seguro?',
@@ -305,37 +316,111 @@ const confirm1 = (event, data, option) => {
             acceptClass: 'p-button-sm p-button-success',
             rejectLabel: 'No',
             acceptLabel: 'Sí',
-            accept: () => {
-                // formColision.value.details = data.map(obj => obj.idScheduleTime);
-                console.log(formColision.value)
+            accept: async () => {
                 if (option == 'omitAll') {
-                    modhours.value = false
-                    openConflict.value = false
-                    formColision.value.actionType = 5
-                    toast.add({ severity: 'info', group: "customToast", text: 'Se omitieron todas', life: 3000 });
-                } else if (option == 'omitAllDay') {
-                    formColision.value.actionType = 4
-                    formColision.value.details = data.map(obj => obj.idScheduleTime);
-                    toast.add({ severity: 'info', group: "customToast", text: 'Se omitieron todas las del dia', life: 3000 });
+                    let error
+                    data.forEach((scheduleD) => {
+                        scheduleD.forEach((scheduleT) => {
+                            if (scheduleT.status == null) {
+                                formColision.value.actionType = 2
+                                formColision.value.scheduleTime = scheduleT.idScheduleTime
+                                let status = resolveCollision(formColision)
+                                if (!status) {
+                                    error = scheduleT
+                                    toast.add({ severity: 'error', group: "error", text: 'Hubo un error inesperado', life: 3000 });
+                                } else {
+                                    scheduleT.status = 'remplace'
+                                }
+                            }
+                        })
+                    })
+                    if (error) {
+                        toast.add({ severity: 'warn', group: "customToast", text: 'Se remplazaron todas con errores', life: 3000 });
+                    } else {
+                        toast.add({ severity: 'success', group: "customToast", text: 'Se remplazaron todas', life: 3000 });
+                    }
                 } else if (option == 'remplaceAll') {
-                    modhours.value = false
-                    openConflict.value = false
-                    formColision.value.actionType = 6
-                    toast.add({ severity: 'info', group: "customToast", text: 'Se remplazaron todas', life: 3000 });
+                    let error
+                    data.forEach((scheduleD) => {
+                        scheduleD.forEach((scheduleT) => {
+                            if (scheduleT.status == null) {
+                                formColision.value.actionType = 1
+                                formColision.value.scheduleTime = scheduleT.idScheduleTime
+                                let status = resolveCollision(formColision)
+                                if (!status) {
+                                    error = scheduleT
+                                    toast.add({ severity: 'error', group: "error", text: 'Hubo un error inesperado', life: 3000 });
+                                } else {
+                                    scheduleT.status = 'remplace'
+                                }
+                            }
+                        })
+                    })
+                    if (error) {
+                        toast.add({ severity: 'warn', group: "customToast", text: 'Se remplazaron todas con errores', life: 3000 });
+                    } else {
+                        toast.add({ severity: 'success', group: "customToast", text: 'Se remplazaron todas', life: 3000 });
+                    }
+                } else if (option == 'omitAllDay') {
+                    let error
+                    await data.forEach((scheduleT) => {
+                        if (scheduleT.status == null) {
+                            formColision.value.actionType = 2
+                            formColision.value.scheduleTime = scheduleT.idScheduleTime
+                            let status = resolveCollision(formColision)
+                            if (!status) {
+                                error = scheduleT
+                                toast.add({ severity: 'error', group: "error", text: 'Hubo un error inesperado', life: 3000 });
+                            } else {
+                                scheduleT.status = 'omit'
+                            }
+                        }
+                    })
+                    if (error) {
+                        toast.add({ severity: 'warn', group: "customToast", text: 'Se omitieron todas las del dia con errores', life: 3000 });
+                    } else {
+                        toast.add({ severity: 'success', group: "customToast", text: 'Se omitieron todas las del dia', life: 3000 });
+                    }
                 } else if (option == 'remplaceAllDay') {
-                    formColision.value.actionType = 3
-                    formColision.value.details = data.map(obj => obj.idScheduleTime);
-                    toast.add({ severity: 'info', group: "customToast", text: 'Se remplazaron todas las del dia', life: 3000 });
+                    let error
+                    data.forEach((scheduleT) => {
+                        if (scheduleT.status == null) {
+                            formColision.value.actionType = 1
+                            formColision.value.scheduleTime = scheduleT.idScheduleTime
+                            let status = resolveCollision(formColision)
+                            if (!status) {
+                                error = scheduleT
+                                toast.add({ severity: 'error', group: "error", text: 'Hubo un error inesperado', life: 3000 });
+                            } else {
+                                scheduleT.status = 'remplace'
+                            }
+                        }
+                    })
+                    if (error) {
+                        toast.add({ severity: 'warn', group: "customToast", text: 'Se remplazaron todas las del dia con errores', life: 3000 });
+                    } else {
+                        toast.add({ severity: 'success', group: "customToast", text: 'Se remplazaron todas las del dia', life: 3000 });
+                    }
+
                 }
             },
             reject: () => {
 
-                toast.add({ severity: 'error', group: "customToast", text: 'You have rejected', life: 3000 });
+                // toast.add({ severity: 'error', group: "customToast", text: 'You have rejected', life: 3000 });
             }
         });
     }
-    console.log(formColision.value)
 };
+
+async function resolveCollision(form) {
+    // await axios.get(route('programming.collisions', form)).then((r) => {
+    //      return true
+    // }).catch((e) => {
+    //     console.log(e)
+    //     return false
+    // })
+    return true
+}
 
 //#endregion
 
@@ -660,16 +745,17 @@ const confirm1 = (event, data, option) => {
                         <span v-if="conflictForDay.length > 1"
                             class="flex p-2 justify-end items-center gap-2  w-[400px]">
                             <Button class="!w-full" label="Reemplazar todo" severity="contrast"
-                                @click="confirm1($event, conflictForDay, 'remplaceAllDay')" />
+                                @click="confirm1($event, null, 'remplaceAllDay', conflictForDay)" />
                             <Button class="!w-full" label="Omitir todo" severity="success"
-                                @click="confirm1($event, conflictForDay, 'omitAllDay')" />
+                                @click="confirm1($event, null, 'omitAllDay', conflictForDay)" />
                         </span>
                     </span>
                     <div class="flex flex-col gap-2">
-                        <div v-for="conflict in conflictForDay" class="">
-                            <div class="flex border rounded-md">
-                                <div class="flex justify-between w-full">
-                                    <Breadcrumb
+                        <div v-for="conflict in conflictForDay" class="grid grid-cols-5 border rounded-md"
+                            :class="conflict.status ? conflict.status == 'omit' ? '!bg-green-100' : '!bg-red-100' : ''">
+                            <div class="flex  p-0.5 col-span-4">
+                                <div class="flex justify-between items-center w-full">
+                                    <Breadcrumb :pt="{ root: '!bg-transparent' }"
                                         :model="[{ label: conflict.NombreProyecto, tooltip: 'Nombre del proyecto' }, { label: conflict.nombrePadreTask, tooltip: 'Nombre del proceso' }, { label: conflict.nombreTask, tooltip: 'Nombre de la actividad' }]">
                                         <template #item="item">
                                             <p v-tooltip.bottom="{ value: item.item.tooltip, pt: { text: 'text-center' } }"
@@ -703,12 +789,17 @@ const confirm1 = (event, data, option) => {
                                         </div>
                                     </div>
                                 </div>
-                                <div class="flex p-2 justify-center items-center gap-2 w-[450px]">
-                                    <Button class="!w-full" label="Reemplazar" severity="warning"
-                                        @click="confirm1($event, conflict, 'remplace')" />
-                                    <Button label="Omitir" class="!w-full" severity="success"
-                                        @click="confirm1($event, conflict, 'omit')" />
-                                </div>
+                            </div>
+                            <div v-if="conflict.status == null" class="flex p-2 justify-center items-center gap-2">
+                                <Button class="!w-full" label="Reemplazar" severity="warning"
+                                    @click="confirm1($event, conflict, 'remplace')" />
+                                <Button label="Omitir" class="!w-full" severity="success"
+                                    @click="confirm1($event, conflict, 'omit')" />
+                            </div>
+                            <div v-else class="flex p-2 justify-between items-center">
+                                <p>{{ conflict.status == 'omit' ? 'Sin modificaciones' : 'Remplazada' }}</p>
+                                <Button icon="fa-solid fa-rotate-left" text severity="danger" v-tooltip="'Deshacer'"
+                                    @click="conflict.status = null" rounded />
                             </div>
                         </div>
                     </div>
@@ -717,9 +808,9 @@ const confirm1 = (event, data, option) => {
         </template>
         <template #footer>
             <Button label="Reemplazar todas las coliciones" severity="danger"
-                @click="confirm1($event, conflicts, 'remplaceAll')" />
+                @click="confirm1($event, null, 'remplaceAll',conflicts)" />
             <Button label="Omitir todas las coliciones" severity="success"
-                @click="confirm1($event, conflicts, 'omitAll')" />
+                @click="confirm1($event, null, 'omitAll',conflicts)" />
         </template>
     </CustomModal>
     <!-- #endregion -->
