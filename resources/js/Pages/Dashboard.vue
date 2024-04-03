ac18742dd98fceb0b58c0a53b4b210bdee2c5364
ecantillo <ecantillo@cotecmar.com>
2024-04-03 15:25:06 -0500

    cambiando la visualizacion del dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -28,7 +28,7 @@ const props = defineProps({
                 <QuotesCard class="col-span-1" :showIndicators="false"
                     v-if="$page.props.auth.user.oficina == 'DEPPC'" />
             </div>
-            <Quotes v-if="$page.props.auth.user.oficina == 'DEPPC'" />
+            <Quotes v-if="$page.props.auth.user.oficina !== 'DEPPC'" />
             <Planning v-if="$page.props.auth.user.oficina == 'DEPPC'" :projects="props.projects" />
             <!-- <Tools v-if="hasPermission('tool edit')" :projects="props.projects" /> -->
             <!-- <Projects v-else /> -->
