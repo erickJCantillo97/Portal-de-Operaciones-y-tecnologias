88e5c662bea99aaa08f49d3a70c33103c26f9912
gbuelvas <gbuelvas@cotecmar.com>
2024-04-12 14:09:38 -0500

    Se generan componentes para optimizar programacion

---
+++ resources/js/Pages/Programming/Components/ListPersonDrag.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import ProgressBar from 'primevue/progressbar';
+import Loading from '@/Components/Loading.vue';
+import CustomInput from '@/Components/CustomInput.vue';
+
+defineProps({
+    arrayPersonFilter: {
+        type: Array,
+        default: []
+    },
+})
+
+const loadingPerson = ref(true);
+const personal = ref([])
+const filter = ref('');
+
+
+const getPersonalData = () => {
+    loadingPerson.value = true
+    axios.get(route('get.personal.user')).then((res) => {
+        personal.value = Object.values(res.data.personal)
+        loadingPerson.value = false
+    })
+}
+getPersonalData()
+
+const itemDrag = defineModel('itemDrag', {
+    required: false
+})
+
+
+</script>
+<template>
+    <div class="sm:flex hidden border sm:flex-col h-36 w-full sm:w-52 sm:rounded-lg  divide-y space-y-1 sm:h-full p-1 gap-1">
+        <div class="h-8">
+            <CustomInput v-model:input="filter" type="search" icon="fa-solid fa-magnifying-glass" />
+        </div>
+        <div v-if="loadingPerson" class="w-full h-full flex flex-col justify-center">
+            <Loading class="mt-10 hidden sm:flex" message="Cargando personas" />
+            <ProgressBar mode="indeterminate" class="flex sm:hidden" style="height: 4px" />
+        </div>
+        <!-- zona de drop -->
+        <div v-if="personal.length > 0" oncontextmenu="return false" onkeydown="return false" :key="personal.length"
+            class="sm:overflow-y-auto overflow-x-visible flex sm:block overflow-y-hidden p-1 space-x-1 space-y-0 sm:space-y-1 sm:space-x-0 sm:w-full">
+            <!-- item a arrastrar -->
+            <div v-for="item in personal" :key="item.Nombres_Apellidos" :draggable="true" @dragstart="itemDrag = item"
+                v-tooltip.top="{ value: 'Arrastra hasta la tarea donde asignaras la persona', showDelay: 1000, hideDelay: 300, pt: { text: 'text-center' } }"
+                :class="[(item.Nombres_Apellidos.toUpperCase().includes(filter.toUpperCase()) || item.Cargo.toUpperCase().includes(filter.toUpperCase())) ? '' : '!hidden', arrayPersonFilter.length>0 ? (arrayPersonFilter.find(objeto => objeto.name === item.Nombres_Apellidos) !== undefined ? 'bg-green-200' : 'bg-red-200') : 'bg-red-200']"
+                class="min-w-[25vw] sm:min-w-32 z-10 rounded-xl border border-primary h-full sm:h-16 shadow-md cursor-pointer hover:bg-primary-light hover:ring-1 hover:ring-primary">
+                <div class="flex flex-col h-full justify-center gap-x-1 p-1">
+                    <div class="flex flex-col justify-center">
+                        <p class="text-sm font-semibold truncate text-gray-900">
+                            {{ item.Nombres_Apellidos }}
+                        </p>
+                        <p class="text-xs truncate  text-gray-500">
+                            {{ item.Cargo }}
+                        </p>
+                        <p class="text-xs truncate  text-gray-500">
+                            {{ item.Oficina }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ resources/js/Pages/Programming/Components/TaskProgramming.vue
@@ -0,0 +1,116 @@
+<script setup>
+import { ref } from 'vue';
+import ProgressBar from 'primevue/progressbar';
+import Loading from '@/Components/Loading.vue';
+const date = ref(new Date())
+const tasks = ref({})
+
+const props = defineProps({
+    day: {
+        type: Date,
+        default: []
+    },
+    project: {
+        type: Number
+    }
+})
+
+function format24h(hora) {
+    try {
+        if (hora.length > 5) {
+            return new Date(hora).toLocaleString('es-CO',
+                { hour: '2-digit', minute: '2-digit', hourCycle: 'h23' })
+        } else {
+            return new Date("1970-01-01T" + hora).toLocaleString('es-CO',
+                { hour: '2-digit', minute: '2-digit', hourCycle: 'h23' })
+        }
+    } catch (error) {
+        console.log(error)
+        return 'error'
+    }
+
+}
+
+async function getTaskDay() {
+    tasks.value = { loading: true, data: [] }
+    await axios.post(route('actividadesDeultimonivelPorProyectos', props.project), { date: props.day }).then((res) => {
+        tasks.value.data = res.data
+        tasks.value.loading = false
+    })
+}
+getTaskDay()
+
+defineEmits(['drop'])
+
+</script>
+<template>
+    <div v-if="tasks.loading" class="flex col-span-5 justify-center h-full items-center">
+        <Loading />
+    </div>
+    <div v-else v-for="task in tasks.data" @drop="$emit('drop', task, day)"
+        @dragover.prevent @dragenter.prevent class="sm:h-full w-1/2 sm:w-1/3 sm:max-h-44 p-0.5 float-left"
+        :key="task.name + date.toDateString()">
+        <div class="flex border pb-1 rounded-md border-primary hover:bg-primary-light flex-col justify-between h-full">
+            <div class="h-min w-full">
+                <p v-tooltip="task.name"
+                    class="border-b font-bold trun border-primary h-10 flex items-center justify-center text-xs px-0.5 w-full text-center">
+                    {{ task.name }}
+                </p>
+            </div>
+            <div class="h-full">
+                <div :key="task.name + new Date().toISOString()" class="flex flex-col justify-between h-full py-1">
+                    <p v-tooltip="task.task" class="text-xs px-1 text-center h-min w-full truncate">
+                        {{ task.task }}
+                    </p>
+                    <div class="flex text-xs h-min justify-between px-1 items-center">
+                        <div class="flex h-min cursor-default space-x-1  justify-center">
+                            <p v-tooltip.left="'Fecha Fin'" class="text-xs text-center"
+                                :class="new Date(task.endDate) < date ? 'bg-red-500 rounded-md px-1' : ''">
+                                {{ task.endDate }}
+                            </p>
+                        </div>
+                        <div
+                            class="flex cursor-default bg-success-light px-1 space-x-1 justify-center items-center rounded-md">
+                            <p v-tooltip.left="'Hora inicio'" class=" text-center">
+                                {{ format24h(task.shift?.startShift ?? '') }}
+                            </p>
+                            <p v-tooltip.left="'Hora Fin'" class="text-center">
+                                {{ format24h(task.shift?.endShift ?? '') }}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="h-16 flex items-center w-full">
+                        <div
+                            class="px-2 flex h-full w-full justify-between sm:justify-center items-center overflow-hidden">
+                            <div
+                                class="overflow-x-hidden hover:overflow-x-auto gap-x-1 flex items-center h-full px-2 py-1 max-w-full flex-nowrap">
+                                <i v-if="task.loading"
+                                    class="fa-solid fa-circle-notch font-bold text-3xl animate-spin"></i>
+                                <img v-tooltip.top="{ value: person.name }"
+                                    @click="togglePerson($event, person, task, dates)" v-if="task.employees?.length > 0"
+                                    v-for="person in task.employees" :dragable="true"
+                                    @dragstart="startDrag($event, person, 'edit')"
+                                    :src="person.photo ?? '/images/person-default.png'"
+                                    class="rounded-full min-h-10 h-10 hover:ring-1 hover:ring-primary w-10 min-w-10 object-cover ring-primary-light shadow-md" />
+                                <div v-if="(task.employees?.length == 0) && !task.loading"
+                                    class="flex items-center p-1">
+                                    <p
+                                        class="text-center text-danger rounded-md border-dashed bg-danger-light animate-pulse">
+                                        Sin personal asignado
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="min-w-9 sm:hidden h-full items-center flex justify-end">
+                                <Button text rounded raised class="!w-8" severity="success"
+                                    icon="fa-solid fa-plus"></Button>
+                            </div>
+                        </div>
+                    </div>
+                    <ProgressBar :value="parseFloat(task.percentDone)" class="h-3 mx-1" v-tooltip="'Avance'"
+                        :pt="{ label: 'text-xs font-thin' }">
+                    </ProgressBar>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
