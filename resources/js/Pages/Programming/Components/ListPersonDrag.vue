ee08cb5c50e323df6871d1a944e211137d2ba8b5
gbuelvas <gbuelvas@cotecmar.com>
2024-04-15 08:41:38 -0500

    Se completan funciones de drag, mejoras visuales al cargar personas programadas. se espera funcion para arrastre de tarea a tarea

--- resources/js/Pages/Programming/Components/ListPersonDrag.vue
+++ resources/js/Pages/Programming/Components/ListPersonDrag.vue
@@ -7,7 +7,7 @@ import CustomInput from '@/Components/CustomInput.vue';
 
 defineProps({
     arrayPersonFilter: {
-        type: Array,
+        type: Object,
         default: []
     },
 })
@@ -45,9 +45,12 @@ const itemDrag = defineModel('itemDrag', {
         <div v-if="personal.length > 0" oncontextmenu="return false" onkeydown="return false" :key="personal.length"
             class="sm:overflow-y-auto overflow-x-visible flex sm:block overflow-y-hidden p-1 space-x-1 space-y-0 sm:space-y-1 sm:space-x-0 sm:w-full">
             <!-- item a arrastrar -->
+            <span v-if="arrayPersonFilter.loading" class="">
+                <ProgressBar mode="indeterminate" style="height: 4px"></ProgressBar>
+            </span>
             <div v-for="item in personal" :key="item.Nombres_Apellidos" :draggable="true" @dragstart="itemDrag = item"
                 v-tooltip.top="{ value: 'Arrastra hasta la tarea donde asignaras la persona', showDelay: 1000, hideDelay: 300, pt: { text: 'text-center' } }"
-                :class="[(item.Nombres_Apellidos.toUpperCase().includes(filter.toUpperCase()) || item.Cargo.toUpperCase().includes(filter.toUpperCase())) ? '' : '!hidden', arrayPersonFilter.length>0 ? (arrayPersonFilter.find(objeto => objeto.name === item.Nombres_Apellidos) !== undefined ? 'bg-green-200' : 'bg-red-200') : 'bg-red-200']"
+                :class="[(item.Nombres_Apellidos.toUpperCase().includes(filter.toUpperCase()) || item.Cargo.toUpperCase().includes(filter.toUpperCase())) ? '' : '!hidden', !(arrayPersonFilter.loading)? (arrayPersonFilter.data.programados.find(objeto => objeto.name === item.Nombres_Apellidos) !== undefined ? 'bg-green-200' : 'bg-red-200') : '']"
                 class="min-w-[25vw] sm:min-w-32 z-10 rounded-xl border border-primary h-full sm:h-16 shadow-md cursor-pointer hover:bg-primary-light hover:ring-1 hover:ring-primary">
                 <div class="flex flex-col h-full justify-center gap-x-1 p-1">
                     <div class="flex flex-col justify-center">
