310082188c47011ca2200df7cd3311d263505cd2
rgutierrez <rgutierrez@cotecmar.com>
2024-04-24 07:52:48 -0500

    Colisionando con Erik

--- resources/js/Pages/Programming/DivisionsByProject.vue
+++ resources/js/Pages/Programming/DivisionsByProject.vue
@@ -42,16 +42,50 @@ const divisions = ref([
     short: 'DVPIN'
   },
   {
-    name: 'PINTURA',
-    short: 'DVPIN'
+    name: 'MECANICA',
+    short: 'DVMEC'
   },
   {
-    name: 'PINTURA',
-    short: 'DVPIN'
+    name: 'HABITABILIDAD',
+    short: 'DVHAB'
+  },
+])
+
+const profiles = ref([
+  {
+    id: 1,
+    tooltip: 'Perfil Persona 1',
+    url: 'https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png'
   },
   {
-    name: 'PINTURA',
-    short: 'DVPIN'
+    id: 2,
+    tooltip: 'Perfil Persona 2',
+    url: 'https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png'
+  },
+  {
+    id: 3,
+    tooltip: 'Perfil Persona 3',
+    url: 'https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png'
+  },
+  {
+    id: 4,
+    tooltip: 'Perfil Persona 4',
+    url: 'https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png'
+  },
+  {
+    id: 5,
+    tooltip: 'Perfil Persona 5',
+    url: 'https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png'
+  },
+  {
+    id: 6,
+    tooltip: 'Perfil Persona 6',
+    url: 'https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png'
+  },
+  {
+    id: 7,
+    tooltip: 'Perfil Persona 7',
+    url: 'https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png'
   },
 ])
 
@@ -77,7 +111,6 @@ const toggle = (event, data, fecha) => {
   <div class="mb-2 mt-2 flex w-full justify-between rounded-lg border bg-slate-200 py-4">
     <div class="flex w-[12%] flex-col items-center justify-center">
       <p class="font-semibold uppercase">Proyectos</p>
-      <span class="text-xs font-semibold uppercase">($1.200.400.000)</span>
     </div>
     <div v-for="day in daysOfWeek"
       class="mx-4 flex-1 rounded-lg bg-gray-300 p-1 py-2 text-center font-semibold italic shadow-sm">
@@ -85,8 +118,8 @@ const toggle = (event, data, fecha) => {
       <div class="flex justify-between w-full space-x-2 px-4">
         <div>12/04/2024</div>
         <div class="space-x-2">
-          <Badge v-tooltip.bottom="'Programados'" value="10" />
-          <Badge v-tooltip.bottom="'No Programados'" value="2" severity="danger" />
+          <Badge v-tooltip.bottom="'Programados'" :value="`${Math.ceil(Math.random() * 20)}`" />
+          <Badge v-tooltip.bottom="'No Programados'" :value="`${Math.ceil(Math.random() * 20)}`" severity="danger" />
         </div>
       </div>
     </div>
@@ -99,7 +132,7 @@ const toggle = (event, data, fecha) => {
         <h2 class="text-lg text-primary font-bold">
           {{ truncateString(project.name, 20) }}
         </h2>
-        <span class="text-[0.7rem]">($127 M)</span>
+        <span class="text-[0.7rem]">${{ Math.ceil(Math.random() * 200) }} M</span>
       </div>
 
       <!--CHIPS DE DIVISIONES-->
@@ -108,16 +141,16 @@ const toggle = (event, data, fecha) => {
           class="grid grid-cols-2 gap-2 text-center px-6 border-r border-dashed  min-h-full py-4 border-red-600">
           <div v-for="division in divisions" @click="toggle($event, task, day.date)"
             class="flex cursor-pointer items-center justify-center rounded-lg bg-slate-400 text-center text-white hover:scale-110">
-            <div class="flex h-full w-full items-center rounded-l-lg bg-primary p-1">
-              <span class="font-extrabold text-white">
-                12
+            <div class="flex size-full justify-center items-center rounded-l-lg bg-primary p-1">
+              <span class="font-extrabold text-white text-xs">
+                {{ Math.ceil(Math.random() * 20) }}
               </span>
             </div>
             <div class="space-y-1">
-              <div class="rounded-tr-lg border-b bg-white px-2 font-bold text-black">
+              <div class="rounded-tr-lg border-b bg-white px-2 font-bold text-black text-xs">
                 {{ division.short }}
               </div>
-              <span>$12M</span>
+              <span class="text-xs">${{ Math.ceil(Math.random() * 200) }} M</span>
             </div>
           </div>
         </div>
@@ -129,16 +162,16 @@ const toggle = (event, data, fecha) => {
   <div class="grid h-6 w-full grid-cols-7 rounded-b-lg border border-primary">
     <div class="flex justify-center items-center">
       <span v-tooltip.top="'Costo Total de Proyectos en la Semana'" class="font-semibold text-emerald-500">
-        $127 M
+        ${{ Math.ceil(Math.random() * 200) }} M
       </span>
     </div>
     <div v-for="item in items" class="col-span-1 px-2 justify-center items-center">
       <span v-tooltip.top="'Costo Programado'" class="font-semibold text-emerald-500">
-        $127 M
+        ${{ Math.ceil(Math.random() * 200) }} M
       </span>
       <span>&nbsp;|&nbsp;</span>
       <span v-tooltip.top="'Costo Día Gerencia'" class="font-semibold text-red-500">
-        $230 M
+        ${{ Math.ceil(Math.random() * 200) }} M
       </span>
     </div>
   </div>
@@ -160,33 +193,17 @@ const toggle = (event, data, fecha) => {
 
       <template #option>
         <div class="flex justify-between">
-          <p>{{ divisions[0].name }}</p>
-          <span>$127 M</span>
+          <p class="font-semibold text-primary">{{ divisions[0].name }}</p>
+          <span class="rounded-lg bg-emerald-500 p-1 text-xs text-white font-medium">
+            ${{ Math.ceil(Math.random() * 200) }} M
+          </span>
         </div>
-        <div class="grid grid-flow-col overflow-x-auto">
-          <img v-tooltip="'Perfil Persona 1'"
-            src="https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png"
-            class="border-full size-10 rounded-full border-2 border-green-500 object-cover m-1" alt="personals-profile">
-          <img v-tooltip="'Perfil Persona 2'"
-            src="https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png"
-            class="border-full size-10 rounded-full border-2 border-green-500 object-cover m-1" alt="personals-profile">
-          <img v-tooltip="'Perfil Persona 3'"
-            src="https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png"
-            class="border-full size-10 rounded-full border-2 border-green-500 object-cover m-1" alt="personals-profile">
-          <img v-tooltip="'Perfil Persona 4'"
-            src="https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png"
-            class="border-full size-10 rounded-full border-2 border-green-500 object-cover m-1" alt="personals-profile">
-          <img v-tooltip="'Perfil Persona 5'"
-            src="https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png"
-            class="border-full size-10 rounded-full border-2 border-green-500 object-cover m-1" alt="personals-profile">
-          <img v-tooltip="'Perfil Persona 6'"
-            src="https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png"
-            class="border-full size-10 rounded-full border-2 border-green-500 object-cover m-1" alt="personals-profile">
-          <img v-tooltip="'Perfil Persona 7'"
-            src="https://dthezntil550i.cloudfront.net/f4/latest/f41908291942413280009640715/1280_960/1b2d9510-d66d-43a2-971a-cfcbb600e7fe.png"
-            class="border-full size-10 rounded-full border-2 border-green-500 object-cover m-1" alt="personals-profile">
+        <div class="grid grid-flow-col">
+          <div v-for="profile in profiles" :key="profile.tooltip" class="flex-none overflow-x-auto w-8 m-1">
+            <img v-tooltip="profile.tooltip" :src="profile.url"
+              class="rounded-full border-2 border-green-500 object-cover size-8" :alt="profile.tooltip">
+          </div>
         </div>
-
       </template>
     </ListBox>
   </OverlayPanel>
