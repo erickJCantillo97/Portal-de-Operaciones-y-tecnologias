ce235dd404d0cabb262d9957af4366d4e78ec57e
cospino <cospino@cotecmar.com>
2024-04-25 09:31:34 -0500

    ajuste de shift, ahora se consultan los horarios personalizados del usuario logueado

--- app/Http/Controllers/Personal/ProgrammingController.php
+++ app/Http/Controllers/Personal/ProgrammingController.php
@@ -374,7 +374,7 @@ class ProgrammingController extends Controller
                     'endShift' => new DateTime('1970-01-01T' . $request->endShift . ':00'),
                     'timeBreak' => $request->timeBreak,
                     'status' => false,
-                    'user' => ''
+                    'user' => auth()->user()->num_sap
                 ]);
             }
             $exist = DetailScheduleTime::where('fecha', $detailScheduleTime->fecha)
@@ -455,7 +455,6 @@ class ProgrammingController extends Controller
                 'task' => $request->actionType == 1 ?
                  $this->getSchedule($detailScheduleTime->fecha, $detailScheduleTime->idTask) : 
                  $this->getSchedule($schedule->fecha, $request->idTask)
-
             ]);
         } catch (Exception $e) {
             DB::rollBack();
@@ -471,44 +470,8 @@ class ProgrammingController extends Controller
             //se eliminan el recurso en el gantt
             Assignment::where('event', $schedule->task_id)
                 ->where('resource', $schedule->employee_id)->delete();
-            // dd($schedule);
             ScheduleTime::where('schedule_id', $schedule->id)->delete();
             $schedule->delete();
-            // switch ($request->type) {
-            //         //SOLO EL $request->date
-            //     case 1:
-            //         ScheduleTime::where('schedule_id', $schedule->id)->delete();
-            //         $schedule->delete();
-            //         break;
-            //     case 2:
-            //         //RESTO DE LA ACTIVIDAD
-            //         $schedules = Schedule::where('fecha', '>=', $schedule->fecha)
-            //             ->where('task_id', $schedule->task_id)
-            //             ->where('employee_id', $schedule->employee_id)->pluck('id')->toArray();
-            //         Schedule::whereIn('id', $schedules)->delete();
-            //         ScheduleTime::whereIn('schedule_id', $schedules)->delete();
-            //         break;
-            //         // case 3:
-            //         //     //RANGO DE FECHAS
-            //         //     $schedule = Schedule::where('employee_id', $request->idUser)
-            //         //         ->whereBetween('fecha', [$request->details[0], $request->details[1]])->get();
-            //         //     $itemsToRemove = $schedule->map(function ($item) {
-            //         //         return $item->id;
-            //         //     });
-            //         //     ScheduleTime::whereIn('schedule_id', $itemsToRemove)->delete();
-            //         //     Schedule::whereBetween('fecha', [$request->details[0], $request->details[1]])->delete();
-            //         //     break;
-            //     case 4:
-            //         //FECHAS ESPECIFICAS
-            //         $schedules = Schedule::whereIn('fecha', $request->details)
-            //             ->where('task_id', $schedule->task_id)
-            //             ->where('employee_id', $schedule->employee_id)->pluck('id')->toArray();
-            //         Schedule::whereIn('id', $schedules)->delete();
-            //         ScheduleTime::whereIn('schedule_id', $schedules)->delete();
-            //         break;
-            //     default:
-            //         break;
-            // }
             DB::commit();
 
             return response()->json([
