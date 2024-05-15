a98ac58cf438317f793ade77cd25956ea1756a3f
gbuelvas <gbuelvas@cotecmar.com>
2024-05-15 15:47:00 -0500

    Se completa menu nuevo

--- resources/js/Components/CustomMenuSidebar.vue
+++ resources/js/Components/CustomMenuSidebar.vue
@@ -14,13 +14,13 @@ defineProps({
 <template>
     <div class="min-h-screen bg-gray-100">
         <div
-            class="sidebar min-h-screen w-[3.35rem] overflow-hidden border-r  hover:w-80 hover:bg-white hover:shadow-lg">
+            class="sidebar min-h-screen w-[3.35rem] overflow-hidden border-r  hover:w-80 bg-white hover:shadow-lg">
             <div class="flex h-screen flex-col justify-between pb-16">
                 <div class="flex h-12 items-center justify-center w-full shadow-md">
                     <ApplicationLogo class="w-10 mb-2" :letras="true" />
                 </div>
-                <ul class="mt-2 space-y-2 tracking-wide h-full overflow-hidden hover:overflow-y-auto">
-                    <li class="min-w-max">
+                <ul class="mt-2 space-y-2 tracking-wide h-full overflow-hidden hover:overflow-y-scroll">
+                    <li class="min-w-max px-1">
                         <span v-for="item in items">
                             <CustomItemMenuSidebar :item :primary="true"></CustomItemMenuSidebar>
                         </span>
--- resources/js/Components/sections/CustomItemMenuSidebar.vue
+++ resources/js/Components/sections/CustomItemMenuSidebar.vue
@@ -6,30 +6,37 @@ defineProps({
     item: Object,
     primary: Boolean
 })
-const open=ref()
+const open = ref(false)
+const load = ref(false)
 </script>
 
 <template>
     <label>
-        <input v-model="open" class="peer/showLabel absolute scale-0" type="checkbox" />
-        <span
-            class="block max-h-12 overflow-hidden rounded-lg transition-all duration-300 peer-checked/showLabel:max-h-full">
-            <Link v-if="item.href" :href="route(item.href)" :aria-label="item.name"
-                class="relative flex cursor-pointer items-center space-x-5 hover:bg-primary-light px-4 py-3 text-primary">
-            <i class="h-5 w-5" :class="[item.icon, 'text-primary']" />
-            <span :class="['-mr-1 font-medium', { 'font-semibold': item.children || primary }]">
-                {{ item.name }}
+        <input v-model="open" class="absolute scale-0" type="checkbox" />
+        <span :class="open ? 'max-h-[100vh] flex flex-col' : 'max-h-12 flex flex-col h-12'"
+            class="overflow-hidden rounded-lg transition-all duration-1000 ease-in-out">
+            <Link v-if="item.href" :href="route(item.href)" :aria-label="item.name" @click="load = true"
+                class="flex justify-between cursor-pointer items-center hover:bg-primary-light px-4 py-3 text-primary">
+            <div class="flex space-x-5 items-center">
+                <i class="h-5 w-5" :class="[item.icon, 'text-primary']" />
+                <span :class="['-mr-1 font-medium', { 'font-semibold': item.children || primary }]">
+                    {{ item.name }}
+                </span>
+            </div>
+            <i v-tooltip="'En desarrollo'" v-if="item.dev" class="w-4 fa-solid fa-circle-exclamation text-danger"></i>
+            <span :class="load ? 'animate-spin' : 'hidden'">
+                <i class="fa-solid fa-spinner"></i>
             </span>
             </Link>
             <div v-else :aria-label="item.name"
-                class="  relative flex justify-between cursor-pointer items-center hover:bg-primary-light px-4 py-3 text-primary">
-                <div class="flex space-x-5">
+                class="flex justify-between cursor-pointer items-center hover:bg-primary-light px-4 py-3 text-primary">
+                <div class="flex space-x-5 items-center">
                     <i class="h-5 w-5" :class="[item.icon, 'text-primary']" />
                     <span :class="['-mr-1 font-medium text-left', { 'font-semibold': item.children || primary }]">
                         {{ item.name }}
                     </span>
                 </div>
-                <span :class="open?'rotate-90':'rotate-0'">
+                <span class="ease-in transition-all duration-200" :class="open ? 'rotate-90' : 'rotate-0'">
                     <i class="fa-solid fa-chevron-right"></i>
                 </span>
             </div>
