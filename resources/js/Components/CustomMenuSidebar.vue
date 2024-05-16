8c59ab837dcba59f18352257547c5260701e207c
gbuelvas <gbuelvas@cotecmar.com>
2024-05-16 08:20:25 -0500

    Se agrega funcion de cerrar los items de menusidebar al salir del menu

--- resources/js/Components/CustomMenuSidebar.vue
+++ resources/js/Components/CustomMenuSidebar.vue
@@ -11,12 +11,13 @@ defineProps({
     }
 })
 const menu = ref(false)
+const closed =ref(false)
 </script>
 <template>
     <Button icon="fa-solid fa-bars" class="text-2xl top-7 fixed lg:hidden" text @click="menu = !menu" />
     <div class="fixed inset-0 bg-[black]/60 z-10" :class="{ hidden: !menu }" @click="menu = !menu"></div>
     <div class="min-h-screen bg-gray-100 fixed z-20 sm:block" :class="{ hidden: !menu }">
-        <div :class="menu?'w-80':'hover:w-80'"
+        <div :class="menu?'w-80':'hover:w-80'" @mouseleave="closed=false" @mouseenter="closed=true"
             class="sidebar min-h-screen w-[3.35rem] overflow-hidden border-r bg-white hover:shadow-lg">
             <div class="flex h-screen flex-col justify-between pb-16">
                 <div class="flex h-12 items-center justify-center w-full shadow-md">
@@ -25,7 +26,7 @@ const menu = ref(false)
                 <ul class="mt-2 space-y-2 tracking-wide h-full overflow-hidden hover:overflow-y-scroll">
                     <li class="min-w-max px-1">
                         <span v-for="item in items">
-                            <CustomItemMenuSidebar :item :primary="true"></CustomItemMenuSidebar>
+                            <CustomItemMenuSidebar :closed :item :primary="true"></CustomItemMenuSidebar>
                         </span>
                     </li>
                 </ul>
